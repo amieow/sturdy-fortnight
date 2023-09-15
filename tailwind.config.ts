0ef5a120b3dee97f802fb027a8f0cd235ca091be
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2023-09-15 15:08:16 +0800

    add prettier, color, font-family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      poppins: ['var(--font-poppins)'],
+      inter: ['var(--font-inter)'],
+    },
+    colors: {
+      primary: "#FF8000",
+      primarySurface: "#FFE6CC",
+      primaryBorder: "#FFD5AA",
+      primaryHover: "#D46B00",
+      primaryPressed: "#AA5500",
+      neutral01: "#FEFEFE",
+      neutral02: "#D4D4D4",
+      neutral03: "#999999",
+      neutral04: "#666666",
+      neutral05: "#444444",
+      neutral06: "#222222",
+      accent: "#69C4FA",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
