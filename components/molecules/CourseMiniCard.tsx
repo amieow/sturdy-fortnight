1edd43078ac3ef3a0df530e05c6690d9e1cb93e5
amieow <109803246+amieow@users.noreply.github.com>
2023-09-19 23:41:36 +0700

    add star icons

--- components/molecules/CourseMiniCard.tsx
+++ components/molecules/CourseMiniCard.tsx
@@ -2,6 +2,8 @@ import { Card, CardContent } from "@/components/atoms/Card";
 import Typography from "@/components/atoms/Typography";
 import { formatRupiah } from "@/lib/formatter";
 import Image from "next/image";
+import { IconStar } from "../atoms/icon";
+
 
 export type CourseMiniCardType = {
   imageUrl: string;
@@ -11,6 +13,7 @@ export type CourseMiniCardType = {
   rating: number;
   reviewCount: number;
 };
+const MAX_STAR_REVIEW = 5
 
 const CourseMiniCard = ({
   imageUrl,
@@ -51,19 +54,22 @@ const CourseMiniCard = ({
 const Review = ({
   reviewCount,
   rating,
-}: Pick<CourseMiniCardType, "reviewCount" | "rating">) => (
+}: Pick<CourseMiniCardType, "reviewCount" | "rating">) => {
+  const minStar = Math.min(MAX_STAR_REVIEW,rating)
+  return (
   <div className={"flex flex-row gap-3 items-center"}>
     {/*STAR*/}
     <div className={"flex flex-row gap-2"}>
-      <div>*</div>
-      <div>*</div>
-      <div>*</div>
-      <div>*</div>
-      <div>*</div>
+      {Array.from({ length: Math.max(0, (MAX_STAR_REVIEW - minStar)) }, (_,i) => (
+        <IconStar key={i} width={9} className="text-neutral05 fill-neutral05"/>
+      ))}
+      {Array.from({ length: minStar }, (_,i) => (
+        <IconStar width={9} key={i} className="text-primary fill-primary"/>
+      ))}
     </div>
     <Typography size={"textXS"}>({reviewCount})</Typography>
   </div>
-);
+);}
 
 const Price = ({
   discountPrice,
